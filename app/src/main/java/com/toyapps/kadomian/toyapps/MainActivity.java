e576df4a2086e0e5403992c3b9443efff5ceb844
Yar <yarsher.dev@gmail.com>
2018-06-26 09:57:34 -0400

    Create

--- app/src/main/java/com/toyapps/kadomian/toyapps/MainActivity.java
+++ app/src/main/java/com/toyapps/kadomian/toyapps/MainActivity.java
@@ -1,13 +1,25 @@
 package com.toyapps.kadomian.toyapps;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String ACTION_USB_ATTACHED  = "android.hardware.usb.action.USB_DEVICE_ATTACHED";
+    ImageView usbImageView;
+    IntentFilter mChargingIntentFilter;
+    USBBroadcastReciver usbBroadcastReciver;
+
+    Toast mToast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +28,48 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent();
         UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
 
+        usbImageView = (ImageView) findViewById(R.id.USBimageView);
+
+        usbBroadcastReciver = new USBBroadcastReciver();
+
         //android.hardware.usb.action.USB_DEVICE_ATTACHED
-
-
-
     }
+
+
+    public void testNotification(View view){
+        //NotificationUtils.remindUserBecauseCharging(this    );
+
+        mToast = Toast.makeText(this, "Cliked", Toast.LENGTH_SHORT);
+        mToast.show();
+    }
+
+
+
+
+    private class USBBroadcastReciver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            boolean isCharging = (action.equals(Intent.ACTION_POWER_CONNECTED));
+            showCharging(isCharging);
+        }
+    }
+
+    private void showCharging(boolean isChargin){
+
+        if (isChargin){
+            usbImageView.setImageResource(R.drawable.larg_Camera);
+        }else {
+            usbImageView.setImageResource(R.drawable.usb_xl);
+        }
+    }
+
+
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/toyapps/kadomian/toyapps/utils/NotificationUtils.java
@@ -0,0 +1,101 @@
+package com.toyapps.kadomian.toyapps.utils;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.toyapps.kadomian.toyapps.MainActivity;
+import com.toyapps.kadomian.toyapps.R;
+
+/**
+ * Created by 10190270 on 6/26/2018.
+ */
+
+public class NotificationUtils {
+
+    private static final int USB_ATTACHED_NOTIFICATION_ID = 1138;
+
+    private static final int USB_ATTACHED_PENDING_INTENT_ID = 3417;
+
+    private static final int ACTION_IGNORE_PENDING_INTENT_ID = 31217;
+    private static final int ACTION_USB_ATTACHED_PENDING_INTENT_ID = 33217;
+
+    private static final String USB_ATTACHED_NOTIFICATION_CHANNEL_ID = "usb_attached_channel";
+
+
+
+    public static void clearAllNotificatins(Context context){
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancelAll();
+    }
+
+
+    public static void remindUserBecauseCharging(Context context) {
+
+
+        //  (8) Get the NotificationManager using context.getSystemService
+
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        //  (9) Create a notification channel for Android O devices
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(USB_ATTACHED_NOTIFICATION_CHANNEL_ID,
+                    "NotificationChannelName", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, USB_ATTACHED_NOTIFICATION_CHANNEL_ID)
+                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setSmallIcon(R.drawable.small_Camera)
+                .setLargeIcon(largeIcon(context))
+                .setContentTitle("Camera Attached")
+                .setContentText("New USB Camera Attached")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("Click to launch the KA Biometric App"))
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(contentIntent(context))
+                .setAutoCancel(true);
+
+        //  (11) If the build version is greater than JELLY_BEAN and lower than OREO,
+        // set the notification's priority to PRIORITY_HIGH.
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                && Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
+            notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        }
+
+        //  (12) Trigger the notification by calling notify on the NotificationManager.
+        // Pass in a unique ID of your choosing for the notification and notificationBuilder.build()
+        notificationManager.notify(USB_ATTACHED_NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    private static Bitmap largeIcon(Context context) {
+
+        Resources res = context.getResources();
+        Bitmap largeIcon = BitmapFactory.decodeResource(res, R.drawable.small_Camera);
+        return largeIcon;
+    }
+
+
+
+
+
+    private static PendingIntent contentIntent(Context context) {
+
+        Intent startActivityIntent = new Intent(context, MainActivity.class);
+
+
+        return PendingIntent.getActivity(context, ACTION_USB_ATTACHED_PENDING_INTENT_ID,
+                startActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+}
