9ae7069016faf7c964210c87c371f60331921cae
Yar <yarsher.dev@gmail.com>
2018-06-26 08:35:25 -0400

    adding intent-filters to manifest. creating device_filter.xml to include list of the usb devices

--- app/src/main/java/com/toyapps/kadomian/toyapps/MainActivity.java
+++ app/src/main/java/com/toyapps/kadomian/toyapps/MainActivity.java
@@ -1,17 +1,20 @@
 package com.toyapps.kadomian.toyapps;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String ACTION_USB_ATTACHED  = "android.hardware.usb.action.USB_DEVICE_ATTACHED";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        Intent intent = new Intent();
+        UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
 
         //android.hardware.usb.action.USB_DEVICE_ATTACHED
 
