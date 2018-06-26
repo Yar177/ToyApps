19aba70ec6717137d1ddab469eb60d10913e7d97
Yar <yarsher.dev@gmail.com>
2018-06-26 14:27:48 -0400

    USBBroadcastReceiver

---
+++ app/src/main/java/com/toyapps/kadomian/toyapps/sync/USBBroadcastReceiver.java
@@ -0,0 +1,8 @@
+package com.toyapps.kadomian.toyapps.sync;
+
+/**
+ * Created by 10190270 on 6/26/2018.
+ */
+
+public class USBBroadcastReceiver {
+}
--- app/src/main/java/com/toyapps/kadomian/toyapps/utils/NotificationUtils.java
+++ app/src/main/java/com/toyapps/kadomian/toyapps/utils/NotificationUtils.java
@@ -42,18 +42,14 @@ public class NotificationUtils {
     public static void usbConnectedNotification(Context context) {
 
 
-        //  (8) Get the NotificationManager using context.getSystemService
 
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-
-        //  (9) Create a notification channel for Android O devices
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel mChannel = new NotificationChannel(USB_ATTACHED_NOTIFICATION_CHANNEL_ID,
                     "NotificationChannelName", NotificationManager.IMPORTANCE_HIGH);
             notificationManager.createNotificationChannel(mChannel);
         }
-
 
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, USB_ATTACHED_NOTIFICATION_CHANNEL_ID)
                 .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
@@ -66,18 +62,16 @@ public class NotificationUtils {
                 .setContentIntent(contentIntent(context))
                 .setAutoCancel(true);
 
-        //  (11) If the build version is greater than JELLY_BEAN and lower than OREO,
-        // set the notification's priority to PRIORITY_HIGH.
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
                 && Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
             notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
         }
 
-        //  (12) Trigger the notification by calling notify on the NotificationManager.
-        // Pass in a unique ID of your choosing for the notification and notificationBuilder.build()
         notificationManager.notify(USB_ATTACHED_NOTIFICATION_ID, notificationBuilder.build());
     }
+
+
+
 
     private static Bitmap largeIcon(Context context) {
 
@@ -91,9 +85,7 @@ public class NotificationUtils {
 
 
     private static PendingIntent contentIntent(Context context) {
-
         Intent startActivityIntent = new Intent(context, MainActivity.class);
-
 
         return PendingIntent.getActivity(context, ACTION_USB_ATTACHED_PENDING_INTENT_ID,
                 startActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
